94de71a56864e34d6f56ca333114d52723ba0edb
Peter H. Boling <peter.boling@gmail.com>
2026-07-29 00:51:43 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- spec/debug_logging/version_spec.rb
+++ spec/debug_logging/version_spec.rb
@@ -3,8 +3,11 @@ RSpec.describe DebugLogging::Version do
   it_behaves_like "a Version module", described_class
 
   it "executes the version file for coverage without redefining constants" do
-    path = File.expand_path("../../lib/debug_logging/version.rb", __dir__)
-    anonymous_namespace = AnonymousLoader.load(files: path)
+    paths = [
+      File.expand_path("../../lib/debug_logging/version.rb", __dir__),
+      File.expand_path("../../lib/debug_logging/version_gem.rb", __dir__)
+    ].select { |path| File.file?(path) }
+    anonymous_namespace = AnonymousLoader.load(files: paths)
 
     expect(anonymous_namespace::DebugLogging::Version::VERSION).to eq(described_class::VERSION)
   end
